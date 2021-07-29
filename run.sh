ff6e585470803cc9c48bd3525628fb711265ec7b
Kyu-Han Kim <kyuhankim@pathlight.com>
2021-07-29 00:40:43 -0700

    Resolved a pagination token error and added comments

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-git tag -d v2.1.2
-git push --delete origin v2.1.2
+git tag -d v2.1.3
+git push --delete origin v2.1.3
 git add -A
-git commit -m "Resolved Pagination Error" 
-git tag v2.1.2
+git commit -m "Resolved a pagination token error and added comments" 
+git tag v2.1.3
 git push
 git push --tags
 
