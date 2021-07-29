cf8b824dfb74df15a2f6c6d1f487985d02ff7264
Kyu-Han Kim <kyuhankim@pathlight.com>
2021-07-29 01:17:52 -0700

    Revert 'resolved a pagination token error'

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 git tag -d v2.1.3
 git push --delete origin v2.1.3
 git add -A
-git commit -m "Resolved a pagination token error and added comments" 
+git commit -m "Revert 'resolved a pagination token error'"
 git tag v2.1.3
 git push
 git push --tags
