711538b882104868cd5f30d26ca8cbea105bea8a
Kyu-Han Kim <kyuhankim@pathlight.com>
2021-07-28 22:57:11 -0700

    Resolved Pagination Error

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-git tag -d v2.1.1
-git push --delete origin v2.1.1
+git tag -d v2.1.2
+git push --delete origin v2.1.2
 git add -A
-git commit -m "Version of singer from 5.9.0 to 5.10.0"
-git tag v2.1.1
+git commit -m "Resolved Pagination Error" 
+git tag v2.1.2
 git push
 git push --tags
 
